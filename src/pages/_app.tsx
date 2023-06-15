9535bfedeb7ddb37d44fbb4884e3d08bd3e7915a
Gabriel Fernandes Lima <gabriel.fernandes.06@outlook.com>
2023-06-14 21:32:26 -0300

    add favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import theme from '@/Theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
+			<Head>
+				<meta charSet='utf-8' />
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta
+					name='viewport'
+					content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+				/>
+				<meta name='description' content='Description' />
+				<meta name='keywords' content='Keywords' />
+				<title>AluraGeek</title>
+				<link rel='icon' type='image/x-icon' href='/images/favicon.ico'></link>
+			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
