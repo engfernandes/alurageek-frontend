be4327fd0b6c49d9c730ebb4910af1a35f822b81
Gabriel Fernandes Lima <gabriel.fernandes.06@outlook.com>
2023-06-14 21:11:25 -0300

    add firebase project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+  images: {
+    domains: ["flagcdn.com"],
+    unoptimized: true,
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+      loader: "akamai",
+      path: "",
+    },
+  }
 };
 
 module.exports = nextConfig;
